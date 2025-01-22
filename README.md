8a8b1183a42f10b4d43011a72c08c6f38ce4e385
Jorge Henrique <jorgehjv2@gmail.com>
2025-01-22 20:14:03 -0300

    Dia 4 do desafio de 60 dias de python

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - [X] Dia 1 - Olá mundo em python
 - [X] Dia 2 - Calculadora
 - [X] Dia 3 - Verificação de par ou ímpar
+- [X] Dia 4 - Laço de repetição
---
+++ day4/laco_repeticao.py
@@ -0,0 +1,11 @@
+#imprimir os números de 0 a 10
+
+for numero in range(11):
+    print(numero)
+
+#imprimir os mesmos números com o laço while
+
+numero = 0
+while numero < 11:
+    print(numero)
+    numero += 1
